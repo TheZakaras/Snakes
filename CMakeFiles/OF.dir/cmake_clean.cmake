f473aa70f95d6840c1ee786092fe0a6561c9d353
TheZakaras <aayushattri.95@gmail.com>
2016-10-14 18:25:24 -0400

    Adding rest of the files

---
+++ CMakeFiles/OF.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/OF.dir/OF.cpp.o"
+  "OF.pdb"
+  "OF"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/OF.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
